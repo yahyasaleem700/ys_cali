c659e3e9b6fba6f14eb2b94fa8bd410015fe1d3a
yahyasaleem700 <yahyasaleem700@gmail.com>
2026-07-28 17:48:44 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
+// YS_CALI Website JavaScript
+
 console.log("YS_CALI website loaded");
+
+
+// Future expansion:
+// - animations
+// - mobile navigation
+// - signup forms
+// - interactive features
